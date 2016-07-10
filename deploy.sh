f1ba8912140688f4a1bf0caabbe38d14cb164505
John D. Martin III <john.d.martin.iii@gmail.com>
2016-07-10 00:58:44 -0400

    Revert "backup commit"
    This reverts commit ebca2b9a5fd2934ce00648b4effebaedf0941595.

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
-TMP=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
 # Backup push
 git add -v *
 git commit -av -m "backup commit"
 git push -v
 # Build Jekyll site
 jekyll build
-# Move _site to tmp file
-mkdir /tmp/$TMP
-rsync -P -r ./_site/ /tmp/$TMP
-rm -r ./_site
+# Move to tmp file
+mkdir /tmp/jekyll-site
+rsync -P -r ./_site/ /tmp/jekyll-site
 # checkout into gh-pages
 git checkout gh-pages 
-rm -r *
-rsync -P -r /tmp/$TMP .
+rsync -P -r /tmp/jekyll-site/ .
 # commit and push changes
 git commit -av -m "update gh-pages site on `date`"
 git push -v
 # check back into master
 git checkout master
 # Clear temp dir
-rm -r /tmp/$TMP
+#rm -r /tmp/jekyll-site
