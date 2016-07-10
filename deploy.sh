2d8b9008b733df61235ad1b3e134c3de991d3cce
John D. Martin III <john.d.martin.iii@gmail.com>
2016-07-10 00:35:57 -0400

    update site

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-
+# Backup push
+git add -v *
+git commit -av -m "backup commit"
+git push -v
 # Build Jekyll site
 jekyll build
 # Move to tmp file
 mkdir /tmp/jekyll-site
-cp ./_site/* /tmp/jekyll-site/
+rsync -P -r ./_site/ /tmp/jekyll-site
 # checkout into gh-pages
 git checkout gh-pages 
-cp /tmp/jekyll-site/* .
+rm -r *
+rsync -P -r /tmp/jekyll-site/ .
 # commit and push changes
-git commit -a -m "update gh-pages site on `date`"
+git commit -av -m "update gh-pages site on `date`"
+git push -v
+# check back into master
 git checkout master
+# Clear temp dir
+#rm -r /tmp/jekyll-site
