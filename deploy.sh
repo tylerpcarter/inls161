099b515de27b196083f7a61ea01d3c68c65dc132
John D. Martin III <john.d.martin.iii@gmail.com>
2016-07-10 00:44:21 -0400

    update gh-pages site on Sun Jul 10 00:44:21 EDT 2016

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,6 @@ mkdir /tmp/jekyll-site
 rsync -P -r ./_site/ /tmp/jekyll-site
 # checkout into gh-pages
 git checkout gh-pages 
-rm -r *
 rsync -P -r /tmp/jekyll-site/ .
 # commit and push changes
 git commit -av -m "update gh-pages site on `date`"
