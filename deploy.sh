e4bff48e03c427a8e98e70d82a2ef958dc04516a
John D. Martin III <john.d.martin.iii@gmail.com>
2016-07-10 20:45:30 -0400

    backup commit

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
+### FUNCTION: Print warning message. 
+function warning() 
+{ 
+    echo "$*" >&2 
+    exit 1   
+} 
+### FUNCTION: Print error message and exit. 
+function error() 
+{ 
+    echo "$*" >&2 
+    exit 1 
+} 
+
+### Display help text
+if [[ "$1" = [-][hH] || "$1" = [-][hH] || "$1" = [-][-][Hh][Ee][Ll][Pp] ]]
+	then
+		echo -e "Usage:	deploy.sh [source branch] [target branch]"
+		echo -e "Example:	deploy.sh gh-pages"
+		echo -e
+		echo -e "This script should be run in the directory where your Jekyll source files live."
+		echo -e
+		echo -e "  -h, --help		Displays this help screen. "
+    exit 0
+fi
+
+TARGETBRANCH=$1
+SRCBRANCH=`git rev-parse --abbrev-ref HEAD`
 TEMPDIR=`mktemp -d`
 WORKDIR=`pwd`
 # Backup push
@@ -10,20 +37,18 @@ cd /tmp
 mkdir $TEMPDIR
 # Build Jekyll site
 jekyll build -s $WORKDIR -d $TEMPDIR
-#rsync -r ./_site/ $TEMPDIR
-#rm -r ./_site
 # Move back to source dir
 cd $WORKDIR
-# checkout into gh-pages
-git checkout -f gh-pages 
+# checkout into gh-pages 
+git checkout -f $TARGETBRANCH
 rm -r *
-rsync -r $TEMPDIR/ .
+rsync -r $TEMPDIR/ 
 # commit and push changes
 git add -v *
 git commit -av -m "update gh-pages site on `date`"
 git push -v
 # check back into master
-git checkout master
+git checkout $SRCBRANCH
 # Clear temp dir
 rm -r $TEMPDIR
 # Exit
